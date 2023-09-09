f4789e7942aa509f90778299e8b424a6192069ee
huccct <2972223145@qq.com>
2023-09-09 16:19:18 +0800

    feat: 🎸 complete nietaheader

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,13 +4,14 @@ module.exports = {
       alias: {
         map: [
           ['@components', './src/components'],
-          // 添加其他别名映射关系
+          ['@assets', './src/assets'],
         ],
         extensions: ['.js', '.jsx', '.ts', '.tsx', '.json', '.astro'],
       },
     },
   },
-  extends: ['airbnb-base', 'plugin:astro/recommended', 'plugin:astro/jsx-a11y-recommended', 'prettier'],
+  extends: ['plugin:astro/recommended', 'plugin:astro/jsx-a11y-recommended', 'prettier'],
+  parser: '@typescript-eslint/parser',
   // ...
   overrides: [
     {
