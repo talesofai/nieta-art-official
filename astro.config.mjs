f35757707b71782ea7375ebc0896d6cc148d647c
huccct <2972223145@qq.com>
2023-09-13 22:44:14 +0800

    fix: 🐛 fix config path

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://talesofai.github.io/',
-  base: './',
   integrations: [tailwind(), react()],
+  build: {
+    assets: 'static',
+  },
 });
