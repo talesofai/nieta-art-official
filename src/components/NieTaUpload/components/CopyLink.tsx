6eb532a8ff716391b919ca31368a442c9f4f0521
huccct <2972223145@qq.com>
2023-09-21 22:28:17 +0800

    feat: 🎸 init upload & fix some bugs

---
+++ src/components/NieTaUpload/components/CopyLink.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function CopyLink() {
+  return <div>CopyLink</div>;
+}
--- src/components/NieTaUpload/index.tsx
+++ src/components/NieTaUpload/index.tsx
@@ -1,5 +1,28 @@
-import React from 'react';
+import './styles/index.scss';
+import React, { useState } from 'react';
 
 export default function index() {
-  return <div>index</div>;
+  const [activeTab, setActiveTab] = useState(0);
+
+  const changeTab = (index) => {
+    setActiveTab(index);
+  };
+
+  return (
+    <>
+      <div className="tabbar">
+        <button className={`tab ${activeTab === 0 ? 'active' : ''}`} onClick={() => changeTab(0)}>
+          Tab 1
+        </button>
+        <button className={`tab ${activeTab === 1 ? 'active' : ''}`} onClick={() => changeTab(1)}>
+          Tab 2
+        </button>
+      </div>
+
+      {/* <div className="content">
+        {activeTab === 0 && <div>Content 1</div>}
+        {activeTab === 1 && <div>Content 2</div>}
+      </div> */}
+    </>
+  );
 }
