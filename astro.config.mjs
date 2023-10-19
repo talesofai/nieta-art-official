d75bbd3f55520115bfd44afb8b0adc396176fd8c
kinori <kinori.me@gmail.com>
2023-10-19 13:08:49 +0800

    chore: update base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,8 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://nieta.art/official',
-  base: '/official',
+  site: 'https://nieta.art/',
+  base: '/',
   integrations: [tailwind(), react()],
   output: 'static',
   build: {
